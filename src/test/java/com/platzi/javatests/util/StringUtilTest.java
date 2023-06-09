873dea9f5c89ed5b23987de6014304d1681b13ab
David Giraldo <santiagogiraldosuescun@gmail.com>
2023-06-09 15:10:22 -0500

    Hemos hecho varios Test y descubrimos el @Test

--- src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -5,14 +5,24 @@ import org.junit.Test;
 
 public class StringUtilTest {
     @Test
-    public void testRepeat() {
-
-        Assert.assertEquals("HolaHolaHola",StringUtil.repeat("Hola", 3));
+    public void repeat_string_once() {
         Assert.assertEquals("Hola",StringUtil.repeat("Hola", 1));
 
     }
 
+    @Test
+    public void repeat_string_multiple_times() {
+        Assert.assertEquals("HolaHolaHola",StringUtil.repeat("Hola", 3));
+    }
 
+    @Test
+    public void repeat_string_zero_times() {
+        Assert.assertEquals("",StringUtil.repeat("Hola", 0));
+    }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void repeat_string_negative_times() {
+        StringUtil.repeat("Hola", -1);
+    }
 
 }
