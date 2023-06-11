446e52c4502d407eddc9494818363c7a7349c979
David Giraldo <santiagogiraldosuescun@gmail.com>
2023-06-10 22:55:12 -0500

    Este es el commit final, ya terminamos el curso de Testing con java, soy feli

--- src/test/java/com/platzi/javatests/movies/data/MovieRepositoryIntegrationTest.java
+++ src/test/java/com/platzi/javatests/movies/data/MovieRepositoryIntegrationTest.java
@@ -2,14 +2,16 @@ package com.platzi.javatests.movies.data;
 
 import com.platzi.javatests.movies.model.Genre;
 import com.platzi.javatests.movies.model.Movie;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
 
-import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -18,11 +20,12 @@ import static org.junit.Assert.assertThat;
 
 public class MovieRepositoryIntegrationTest {
 
+    private MovieRepositoryJdbc movieRepository;
+    private DriverManagerDataSource dataSource;
 
-    @Test
-    public void load_all_movies_() throws SQLException {
-
-        DataSource dataSource =
+    @Before
+    public void setUp() throws Exception {
+        dataSource =
                 new DriverManagerDataSource(
                         "jdbc:h2:mem:test;MODE=MYSQL", "sa", "sa");
 
@@ -30,7 +33,11 @@ public class MovieRepositoryIntegrationTest {
 
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
 
-        MovieRepositoryJdbc movieRepository = new MovieRepositoryJdbc(jdbcTemplate);
+        movieRepository = new MovieRepositoryJdbc(jdbcTemplate);
+    }
+
+    @Test
+    public void load_all_movies_() throws SQLException {
 
         Collection<Movie> movies =
                 movieRepository.findAll();
@@ -42,4 +49,28 @@ public class MovieRepositoryIntegrationTest {
         )));
 
     }
+
+    @Test
+    public void load_movie_by_id() {
+        Movie movie = movieRepository.findById(2);
+        assertThat(movie, is(new Movie(2, "Memento", 113, Genre.THRILLER)));
+    }
+
+    @Test
+    public void insert_a_movie() {
+        Movie movie = new Movie("Super 8", 112, Genre.THRILLER);
+        movieRepository.saveOrUpdate(movie);
+        Movie movieFromDb = movieRepository.findById(4);
+
+        assertThat(movieFromDb, is(new Movie(4, "Super 8", 112, Genre.THRILLER)));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        // Remove H2 files --
+        final Statement s = dataSource.getConnection().createStatement();
+        s.execute("drop all objects delete files");//"shutdown" is also  enough for men db
+    }
+
+
 }
